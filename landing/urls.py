4724478ef50bac40fd6744a5ec3811f0b3c2a169
amirhosseindehghan1 <dehghanamirhossein1@gmail.com>
2023-07-29 14:19:52 +0330

    Add develop and launch function && url

--- landing/urls.py
+++ landing/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import discovery, market
+from .views import discovery, market, develop_and_launch
 
 
 urlpatterns = [
     path('', discovery),
     path('market/', market),
+    path('market/', develop_and_launch),
     ]
--- landing/views.py
+++ landing/views.py
@@ -7,3 +7,6 @@ def discovery(request):
 
 def market(request):
     return render(request, 'market.html')
+
+def develop_and_launch(request):
+    return render(request, 'develop_and_launch.html')
